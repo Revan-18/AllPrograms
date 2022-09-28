a767eff6bd6ba58be76277a01cf03651a11c6650
revan <gujarevan@gmail.com>
2022-09-28 17:24:05 +0530

    efficient solution of the longest substring without repeating chars

--- src/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ src/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,22 +1,24 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
-        int maxLength = lengthOfLongestSubstring("abcabefgabcd");
+        int maxLength = lengthOfLongestSubstring("dvdf");
         System.out.println(maxLength);
 
     }
     public static int lengthOfLongestSubstring(String s) {
         int maxlength=0;
-        for (int i = 0; i < s.length(); i++) {
-            StringBuilder builder = new StringBuilder();
-            for (int j = i; j < s.length(); j++) {
-                if(builder.indexOf(String.valueOf(s.charAt(j))) != -1){
-                    break;
-                }
-                builder.append(s.charAt(j));
-                maxlength = Math.max(builder.length(), maxlength);
+        Map<Character,Integer> map = new HashMap<>();
+        for (int start = 0,last=0; last < s.length(); last++) {
+            char currentChar = s.charAt(last);
+            if(map.containsKey(currentChar)&&map.get(currentChar)>=start){
+                start=map.get(currentChar)+1;
             }
+            maxlength=Math.max(maxlength,last-start+1);
+            map.put(s.charAt(last),last);
 
         }
         return maxlength;
